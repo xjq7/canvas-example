2e760c81dcb9cfaeabf194a175ab4f3d8bc4e22b
xjq <lebnizjoan@gmail.com>
2024-10-16 14:08:13 +0800

    feat: 增加 github 链接

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import Router from './router';
+import GithubSvg from './assets/github.svg';
 
 export default function Component() {
   return (
     <div>
       <Router />
+      <a
+        className="w-[50px] h-[50px] flex items-center justify-center fixed right-0 top-0 cursor-pointer"
+        href="https://github.com/xjq7/canvas-example"
+        target="_blank"
+      >
+        <img src={GithubSvg} className="w-[40px] h-[40px]" />
+      </a>
     </div>
   );
 }
